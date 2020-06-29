52747a00a255e34775450a89efc4d62712f862f5
DrRico <851774781@qq.com>
2020-06-29 15:50:41 +0800

    添加完成所有功能的按键，并为所有的按键添加样式和图标

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog,QMessageBox
 from PyQt5.QtGui import *
 from position_btn import Ui_Form
 
-
 class DetailUI(Ui_Form,QMainWindow):
     def __init__(self):
         super(DetailUI, self).__init__()
@@ -15,13 +14,9 @@ class DetailUI(Ui_Form,QMainWindow):
         self.mul_img_btn.clicked.connect(self.openFolder)
         self.exit_btn.clicked.connect(self.close)
 
-
     def openImage(self):
         try:
             fname , _ = QFileDialog.getOpenFileName(self, 'open file', r'D:\Python_Shit\My_shit\TXEDU\Learning_SVM\My_HOG_LBP_SVM\mate1283',"Image files (*.jpg *.gif *.png)")
-            #self.angle_test.setText(str(fname))
-            #img = QPixmap(fname)
-            #print(img)
             self.test_img_show.setPixmap(QPixmap(fname))
             self.test_img_show.setScaledContents(True)
 
